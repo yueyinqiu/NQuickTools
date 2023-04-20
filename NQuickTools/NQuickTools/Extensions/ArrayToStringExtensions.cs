651e0786aa4c0512919f3d37b3ec95a5425eab9c
yueyinqiu <lujiaqi2003@126.com>
2023-04-21 00:57:15 +0800

    nqt

---
+++ NQuickTools/NQuickTools/Extensions/ArrayToStringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NQuickTools.Extensions
+{
+    public static class ArrayToStringExtensions
+    {
+        public static string ArrayToString<T>(this IEnumerable<T> array)
+        {
+            return $"[{string.Join(", ", array)}]";
+        }
+
+        public static string ToBase64String(this byte[] array)
+        {
+            return Convert.ToBase64String(array);
+        }
+        public static string ToHexString(this byte[] array)
+        {
+            return Convert.ToHexString(array);
+        }
+    }
+}
